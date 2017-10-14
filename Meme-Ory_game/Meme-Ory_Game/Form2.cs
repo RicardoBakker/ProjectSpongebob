a90c9ee9bcc0c6c612148f1211afbb01a6b31bae
FabiandeWilde <wild1704@student.nhl.nl>
2017-10-14 11:46:42 +0200

    FabianV0.5

--- Meme-Ory_game/Meme-Ory_Game/Form2.cs
+++ Meme-Ory_game/Meme-Ory_Game/Form2.cs
@@ -18,12 +18,13 @@ namespace Meme_Ory_Game
         {
             InitializeComponent();
         }
+    
 
         private void Form2_Load(object sender, EventArgs e)
         {
             int[] nummers = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             int[] randomarray = random(nummers);        // Running 'Random' method from below to fix position numbers
-
+            
             int Row = 4;
             int Column = 4;
             tableLayoutPanel1.RowCount = Row;
@@ -64,57 +65,39 @@ namespace Meme_Ory_Game
             
             switch (nummer)
             {
-                //elke knop krijgt andere image. button_1 en button_2 = paar etc...
-                
-                
+                //elk paar krijgt dezelfde achtergrond via button_Click
                 case 1:
-                    Image image1 = Properties.Resources.image_1;
-                    return image1;
                 case 2:
+                    Image image1 = Properties.Resources.image_2;
+                    return image1;
+                case 3:
+                case 4:
                     Image image2 = Properties.Resources.image_2;
                     return image2;
-                case 3:
+                case 5:
+                case 6:
                     Image image3 = Properties.Resources.image_3;
                     return image3;
-                case 4:
+                case 7:
+                case 8:
                     Image image4 = Properties.Resources.image_4;
                     return image4;
-                case 5:
+                case 9:
+                case 10:
                     Image image5 = Properties.Resources.image_5;
                     return image5;
-                case 6:
+                case 11:
+                case 12:
                     Image image6 = Properties.Resources.image_6;
                     return image6;
-                case 7:
+                case 13:
+                case 14:
                     Image image7 = Properties.Resources.image_7;
                     return image7;
-                case 8:
+                case 15:
+                case 16:
                     Image image8 = Properties.Resources.image_8;
                     return image8;
-                case 9:
-                    Image image9 = Properties.Resources.image_9;
-                    return image9;
-                case 10:
-                    Image image10 = Properties.Resources.image_10;
-                    return image10;
-                case 11:
-                    Image image11 = Properties.Resources.image_11;
-                    return image11;
-                case 12:
-                    Image image12 = Properties.Resources.image_12;
-                    return image12;
-                case 13:
-                    Image image13 = Properties.Resources.image_13;
-                    return image13;
-                case 14:
-                    Image image14 = Properties.Resources.image_14;
-                    return image14;
-                case 15:
-                    Image image15 = Properties.Resources.image_15;
-                    return image15;
-                case 16:
-                    Image image16 = Properties.Resources.image_16;
-                    return image16;
                 default:
                     Image dickbutt = null;
                     return dickbutt;
